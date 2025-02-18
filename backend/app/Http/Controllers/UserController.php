f452691eecbebe35afa8c5686849740b289e6dca
Victor Santana <134336920+VitalHugal@users.noreply.github.com>
2025-02-18 16:19:57 -0300

    update

--- backend/app/Http/Controllers/UserController.php
+++ backend/app/Http/Controllers/UserController.php
@@ -47,11 +47,9 @@ class UserController extends Controller
 
             $matchIds = is_array($getAllMatchs) ? $getAllMatchs : collect($getAllMatchs)->pluck('id')->toArray();
 
-            $getAllUsers = User::where('id', array_merge($userRequest->id, $matchIds, $getAllUsersLike))
+            $getAllUsers = User::whereNotIn('id', array_merge([$userRequest->id], $matchIds, $getAllUsersLike))
                 ->whereIn('fk_gender_user_id', $preference)
                 ->where('level', 0)
-                ->where('id', '!=', $userRequest->id)
-                // ->orderByRaw('RAND()')
                 ->inRandomOrder()
                 ->get();
 
