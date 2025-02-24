e16def67b674f9b09180e31aa244140f2cb7a2b1
Lucas dos Anjos <lucas.anjos@bizsys.com.br>
2025-02-24 12:59:23 -0300

    feat: page /cta ok

--- frontend/src/pages/cta/index.jsx
+++ frontend/src/pages/cta/index.jsx
@@ -1,7 +1,7 @@
 // Funcionalidades / Libs:
-// import Cookies from "js-cookie";
+import Cookies from "js-cookie";
 import { useState, useEffect } from "react";
-import { Link } from "react-router";
+import { useNavigate } from "react-router";
 
 // Contexts:
 // import UserContext from "../../contexts/userContext";
@@ -12,10 +12,10 @@ import { Link } from "react-router";
 // Utils
 
 // Assets:
-// import imgLogo from '../../assets/LOGO-BIZSYS_preto.png';
+import imgLogo from '../../assets/Logo.png'
 
 // Estilo:
-// import './style.css';
+import './style.css';
 
 
 
@@ -25,7 +25,18 @@ export default function Cta() {
     
 
     // Logica da UI
+    const instructions = [
+        '1',
+        '2',
+        '3',
+        '4'
+    ];
+    const [step, setStep] = useState(0);
+    const [animateMode, setAnimateMode] = useState('');
 
+
+    const navigate = useNavigate();
+    const termosCookie = Cookies.get('termos_jc');
 
 
     useEffect(()=> {
@@ -38,19 +49,77 @@ export default function Cta() {
     
 
     
-  
+    
+    function handleNextStep() {
+        setAnimateMode('');
+
+        setTimeout(()=> {
+            setAnimateMode('animate__fadeInRight animate__fast');
+            setStep(step + 1);
+        }, 100);
+    }
+    function handleBackStep() {
+        setAnimateMode('');
+
+        setTimeout(()=> {
+            setAnimateMode('animate__fadeInLeft animate__fast');
+            setStep(step - 1);
+        }, 100);
+    }
+
+    function handleDirectingRoute() {
+        if(termosCookie) {
+            navigate('/login');
+        }
+        else {
+            //=// navigate('/terms');
+            navigate('/login');
+        }
+    }
+
 
   
     return (
         <div className="Page">
             
-            <main className='PageContent grid'>
+            <main className='PageContent CtaContent grid'>
                 <div className="title_page">
-                    <h1>LOGO</h1>
+                    <img src={imgLogo} alt="" />
                 </div>
 
-                <div>
-                    CTA
+                <div className="content_main">
+                    <h3>Breve explicação do web app:</h3>
+
+                    <div className="slider">
+                        <div className="instructions">
+                            <h2 className={`animate__animated ${animateMode}`}>STEP {instructions[step]}</h2>
+                        </div>
+
+                        <div className="control">
+                            <button className={`btn ${step == 0 ? 'hidden' : ''}`} onClick={handleBackStep}>
+                                <i className="bi bi-caret-left-fill"></i>
+                                <span>Anterior</span>
+                            </button>                        
+
+                            {step < instructions.length-1 ? (
+                            <button className="btn" onClick={handleNextStep}>
+                                <span>Próximo</span>
+                                <i className="bi bi-caret-right-fill"></i>
+                            </button>
+                            ) : (
+                            <button className="btn" onClick={handleDirectingRoute}>
+                                Bora começar!
+                            </button>
+                            )}
+                        </div>
+                    </div>
+
+                    <div className="skip">
+                        <button className="btn" onClick={handleDirectingRoute}>
+                            <span>Pular</span>
+                            <i className="bi bi-arrow-right-short"></i>
+                        </button>
+                    </div>
                 </div>
             </main>
 
